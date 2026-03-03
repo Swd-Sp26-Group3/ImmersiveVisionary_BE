be1cdc945675137fc47569668c0a0dee673ce3b2
Võ  Phú <phuvo1104@gmail.com>
2026-03-03 15:45:54 +0700

    Revert "del"
    This reverts commit 84cc9f9cb531def72fe98bc02d83371668675cbf.

---
+++ ImmersiveVisionary/Infrastructure/Persistence/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
